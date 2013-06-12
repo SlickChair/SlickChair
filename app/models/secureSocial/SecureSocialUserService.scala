e4014c7f619a490e0a9d64b31f4c94fa913d8fac
OlivierBlanvillain <olivier.blanvillain@gmail.com>
2013-06-13 00:38:30 +0300

    Update SecureSocialUserService.scala

--- app/models/secureSocial/SecureSocialUserService.scala
+++ app/models/secureSocial/SecureSocialUserService.scala
@@ -3,6 +3,10 @@ package models.secureSocial
 import play.api._
 import securesocial.core._
 
+/**
+ * Implements the UserServicePlugin required to use the SecureSocial plugin on top
+ * of Slick. See http://securesocial.ws/guide/user-service.html for more details.
+ */
 class SecureSocialUserService(application: Application) extends UserServicePlugin(application)
   with SecureSocialUsers.Queries
   with SecureSocialTokens.Queries
