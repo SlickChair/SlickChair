df9aa8bf919c4dddda42d4b42b9be90ffc03e335
olivier <olivier@ThinkPad.(none)>
2013-08-08 08:10:15 +0200

    Documentation for Settings.scala done.

--- app/models/utils/Settings.scala
+++ app/models/utils/Settings.scala
@@ -15,21 +15,38 @@ import play.api.Play.current
   * @tparam T  type of the setting
   */
 trait Setting[T] {
-  // Hack. (I have no idea how portable this is)
-  /**
-    * Com
+  /** Extract a unique String identifier of the Setting used to identify this
+    * Setting in the database.
     */
-  private def dbname: String = this.getClass.getName.split("$")(0)
+  private def dbname: String = 
+    // Hack. (I have no idea how portable this is)
+    this.getClass.getName.split("$")(0)
+  
+  /** De-serializes the Setting value from it's string representation. */
   protected def fromString(s: String): T
+  
+  /** Default value of the Setting. */
   def default: T
+  
+  /** Description of the Setting. */
   def description: String
   
+  /** Gets the Setting value from the database. If the corresponding entry is
+    * not found, return the default value.
+    *
+    * @return the value of the Setting
+    */
   def get: T = DB.withSession{implicit session =>
     Query(Settings).filter(_.name is dbname).list.headOption match {
       case Some(DBSetting(_, value)) => fromString(value)
       case None => default
     }
   }
+  
+  /** Sets the Setting value in the database.
+    *
+    * @param  value  the value of the Setting
+    */
   def set(value: T): Unit = DB.withTransaction { implicit session =>
     val entry = DBSetting(dbname, value.toString)
     Query(Settings).filter(_.name is dbname).list.headOption match {
@@ -40,16 +57,20 @@ trait Setting[T] {
   }
 }
 
+/** Boolean Setting */
 case class BooleanSetting(default: Boolean, description: String) extends Setting[Boolean] {
   override def fromString(s: String) = s.toBoolean
 }
+/** Int Setting */
 case class IntSetting(default: Int, description: String) extends Setting[Int] {
   override def fromString(s: String) = s.toInt
 }
+/** String Setting */
 case class StringSetting(default: String, description: String) extends Setting[String] {
   override def fromString(s: String) = s
 }
 
+/** TODO: ScalaDoc us! */
 object SubmissionLock extends BooleanSetting(
   false, "Prevent user from posting new submissions")
 // CanMakeSubmission
@@ -66,7 +87,6 @@ object ConferenceShortName extends StringSetting(
 )
 
 case class DBSetting (name: String, value: String)
-
 object Settings extends Table[DBSetting]("SETTINGS") {
   def name = column[String]("NAME", O.DBType("TEXT"), O.PrimaryKey)
   def value = column[String]("VALUE", O.DBType("TEXT"))
