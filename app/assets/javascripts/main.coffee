e8f5ad363d86335fa779eb1d5bb39bc06d71d209
olivier <olivier.blanvillain@gmail.com>
2013-09-13 09:06:36 +0200

    Setup a custom plugin for SecureSocial templates.

--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -1,7 +1,8 @@
 $ ->
-  # Bootstrap file selector. Inspired from http://blog.anorgan.com/2012/09/30
-  # /pretty-multi-file-upload-bootstrap-jquery-twig-silex/ and
-  # http://jasny.github.io/bootstrap/javascript.html#fileupload
+  ### Bootstrap file selector. Inspired by
+      http://blog.anorgan.com/2012/09/30/pretty-multi-file-upload-bootstrap-jquery-twig-silex/
+      and http://jasny.github.io/bootstrap/javascript.html#fileupload
+  ###
   if $(".file-selector").length
     $(".file-selector").each ->
       button = $(this).find("button")
@@ -13,12 +14,14 @@ $ ->
         e.preventDefault()
         fileInput.click()
 
-  # Hides/show javascript managed elements.
+
+  ### Hides/show javascript managed elements. ###
   $(".hidejs").each -> $(this).hide()
   $(".showjs").each -> $(this).show()
-  $("#author0").show()
   
-  # Handles plus and minus buttons.
+  
+  ### Handles plus and minus buttons. ###
+  $("#author0").show()
   nauthors = $("#nauthors")
   getNauthors = -> Number(nauthors.val())
   setNauthors = (n) ->
@@ -29,8 +32,6 @@ $ ->
       false
     else
       true
-  incNauthors = -> setNauthors(getNauthors() + 1)
-  decNauthors = -> setNauthors(getNauthors() - 1)
   
   disableMinusIfZero = ->
     if getNauthors() == 1
@@ -40,25 +41,28 @@ $ ->
   disableMinusIfZero()
   
   $(".glyphicon-plus-sign").click (e) ->
-    if incNauthors()
+    if setNauthors(getNauthors() + 1)
       $("#author#{ getNauthors() - 1 }").show()
     disableMinusIfZero()
-    
+  
   $(".glyphicon-minus-sign").click (e) ->
-    if decNauthors()
+    if setNauthors(getNauthors() - 1)
       $("#author#{ getNauthors() }").hide()
     disableMinusIfZero()
   
-  # /slq, select the query textarea and submit on ctrl+enter.
+  
+  ### /slq, select the query textarea and submit on ctrl+enter. ###
   $("#query").select()
   $("body").keydown (e) ->
     ENTER = 13
     if e.keyCode == ENTER and e.ctrlKey
       $(".ctrl-enter").find("button").click()
   
-  # Modal login magic.
+  
+  ### Modal login magic. ###
   $(".email").click (e) ->
     $(".email-form").toggle()
+    $(".note").toggle()
     $(".facebook").toggleClass("half-transparent")
     $(".google").toggleClass("half-transparent")
   
---
+++ app/controllers/SecureSocialTemplates.scala
@@ -0,0 +1,66 @@
+package controllers
+
+import play.api.mvc.{RequestHeader, Request}
+import play.api.templates.{Html, Txt}
+import play.api.{Logger, Plugin, Application}
+import securesocial.core.{Identity, SecuredRequest, SocialUser}
+import play.api.data.Form
+import securesocial.controllers.Registration.RegistrationInfo
+import securesocial.controllers.PasswordChange.ChangeInfo
+import securesocial.controllers.TemplatesPlugin
+
+class SecureSocialTemplates(application: Application) extends TemplatesPlugin {
+  override def getLoginPage[A](implicit request: Request[A], form: Form[(String, String)],
+                               msg: Option[String] = None): Html =
+  {
+    securesocial.views.html.login(form, msg)
+  }
+
+  override def getSignUpPage[A](implicit request: Request[A], form: Form[RegistrationInfo], token: String): Html = {
+    securesocial.views.html.Registration.signUp(form, token)
+  }
+
+  override def getStartSignUpPage[A](implicit request: Request[A], form: Form[String]): Html = {
+    securesocial.views.html.Registration.startSignUp(form)
+  }
+
+  override def getStartResetPasswordPage[A](implicit request: Request[A], form: Form[String]): Html = {
+    securesocial.views.html.Registration.startResetPassword(form)
+  }
+
+  def getResetPasswordPage[A](implicit request: Request[A], form: Form[(String, String)], token: String): Html = {
+    securesocial.views.html.Registration.resetPasswordPage(form, token)
+  }
+
+  def getPasswordChangePage[A](implicit request: SecuredRequest[A], form: Form[ChangeInfo]):Html = {
+    securesocial.views.html.passwordChange(form)
+  }
+
+  def getNotAuthorizedPage[A](implicit request: Request[A]): Html = {
+    securesocial.views.html.notAuthorized()
+  }
+
+  def getSignUpEmail(token: String)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.signUpEmail(token)))
+  }
+
+  def getAlreadyRegisteredEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.alreadyRegisteredEmail(user)))
+  }
+
+  def getWelcomeEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.welcomeEmail(user)))
+  }
+
+  def getUnknownEmailNotice()(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.unknownEmailNotice(request)))
+  }
+
+  def getSendPasswordResetEmail(user: Identity, token: String)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.passwordResetEmail(user, token)))
+  }
+
+  def getPasswordChangedNoticeEmail(user: Identity)(implicit request: RequestHeader): (Option[Txt], Option[Html]) = {
+    (None, Some(securesocial.views.html.mails.passwordChangedNotice(user)))
+  }
+}
